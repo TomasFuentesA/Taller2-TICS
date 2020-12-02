41b62786b041e76ee9127d3cd2a6cc30164105ed
Javier Ramos <javier.ramos1@mail.udp.cl>
2020-12-02 13:45:27 -0300

    vista sensores implementada, base de datos migrada

--- Regadio/polls/views.py
+++ Regadio/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 
 def index(request):
@@ -12,16 +13,44 @@ def index(request):
 def sensort(request):
     template = loader.get_template('polls/senset.html')
     context ={}
+    sensores = Sensores.objects.filter(tipo="temperatura")
+    ides = []
+    cont = 1
+    for i in sensores:
+        ides.append(cont)
+        cont+=1
+    sensores = zip(ides,sensores)
+    context["sensores"] = sensores
     return HttpResponse(template.render(context,request))
 
 def sensorl(request):
     template = loader.get_template('polls/sensel.html')
     context ={}
+   # Sensor.objects.create(id= asdsad, tipo = asdsad, estado= asdsa) # crear
+   # Sensor.objects.all() Retrieve all objects on Table
+   # Sensor.objects.get(id=1) Retrieve one object
+   # Sensor.objects.filter(estado=0)
+    sensores = Sensores.objects.filter(tipo="luz")
+    ides = []
+    cont = 1
+    for i in sensores:
+        ides.append(cont)
+        cont+=1
+    sensores = zip(ides,sensores)
+    context["sensores"] = sensores
     return HttpResponse(template.render(context,request))
 
 def sensorh(request):
     template = loader.get_template('polls/senseh.html')
     context ={}
+    sensores = Sensores.objects.filter(tipo="humedad")
+    ides = []
+    cont = 1
+    for i in sensores:
+        ides.append(cont)
+        cont+=1
+    sensores = zip(ides,sensores)
+    context["sensores"] = sensores
     return HttpResponse(template.render(context,request))
 
 def graficos(request):
